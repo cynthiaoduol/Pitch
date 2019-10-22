1f2bfaf29546a488962c68a6ce159db453c6f02e
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-22 14:01:09 +0300

    create a route decorator and define a function that returns the index page and its data

---
+++ app/main/views.py
@@ -0,0 +1,11 @@
+from flask import render_template,redirect,url_for
+from. import main
+
+@main.route('/')
+def index():
+    '''
+    root page function that returns the index page and its data
+    '''
+    title = "Welcome | One Minute Pitch"
+
+    return render_template("index.html", title=title)
