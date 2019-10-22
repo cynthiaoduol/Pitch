29ba5385c07c5e77b482f350b11747b27fc93f01
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-22 15:49:18 +0300

    initialize database

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ def create_app(config_name):
 
     #Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     # Registering the main app Blueprint
     from .main import main as main_blueprint
