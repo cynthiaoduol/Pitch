1c337f1b40ac2a1f0e93b4d6ba388d97cfbe55df
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-25 18:06:17 +0300

    push to heroku

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:minutepitch@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
     
     # Email Configurations
     MAIL_SERVER = 'smtp.googlemail.com'
