87e733910f39a6d70f6531cf45dc305544c013a6
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-25 11:58:06 +0300

    set up email confugurations

--- config.py
+++ config.py
@@ -3,19 +3,34 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:minutepitch@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    # Email Configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
     pass
 
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:minutepitch@localhost/pitch'
+
+
 class DevConfig(Config):
-    """Configuration class for development stage of the app"""
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:minutepitch@localhost/pitch'
     DEBUG = True
 
 
 config_options = {
     'development': DevConfig,
-    'production': ProdConfig
-    #'test':TestConfig
+    'production': ProdConfig,
+    'test':TestConfig
 }
