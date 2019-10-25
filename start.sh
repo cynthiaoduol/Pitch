13bbf39deec0853534996675ad4bdcfd92353e96
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-25 12:00:15 +0300

    add export code for mail username and mail password

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export SECRET_KEY='21038646d'
-export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:minutepitch@localhost/pitch'
+export SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringaschool:minutepitch@localhost/pitch'
+export MAIL_USERNAME='cynthiaobu940@gmail.com'
+export MAIL_PASSWORD='21038646'
 
 python3.6 manage.py server
