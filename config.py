710255225442b4340e441925d0d86d56ffb8863b
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-25 18:31:04 +0300

    push to heroku master

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:minutepitch@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     
