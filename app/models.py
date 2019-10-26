54ad499f00ade59301e644a42cecca7adf8c08d1
cynthiaoduol <cynthiaobu940@gmail.com>
2019-10-27 01:41:00 +0300

    updating the user class

--- app/models.py
+++ app/models.py
@@ -19,7 +19,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), unique=True, index=True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-
     pass_hash = db.Column(db.String(255))
     pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
     comments = db.relationship('Comment', backref='user', lazy='dynamic')
